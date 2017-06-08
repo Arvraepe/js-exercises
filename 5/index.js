dc33ccf3dc024bae25e66e519e88044f2bccd292
Arne Van Raepenbusch <arvraepe@gmail.com>
2017-06-08 08:32:36 +0200

    Added a small exercise

--- 5/index.js
+++ 5/index.js
@@ -5,5 +5,10 @@
  */
 
 /**
- *  Implement a recursive sum that takes an array [1,2,3,4] using the ...rest argument and array destruction.
+ *  Implement a function that calculates the sum recursively. Takes an array [1,2,3,4] and use the ...rest argument and array destruction.
+ */
+
+
+/**
+ *  Implement a function that switches the first and second element of an array [2,1,3,4] to [1,2,3,4] (use the ... operator)
  */
